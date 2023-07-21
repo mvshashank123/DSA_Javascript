65b05c3f54cd4b79a374938ec3ce1c9fa43cb94b
Shashank M V <mv.shashank123@gmail.com>
2023-07-21 16:52:34 +0530

    Factorial using loops and recursion

---
+++ factorial.js
@@ -0,0 +1,17 @@
+const findFactorial = (num) => {
+    let fact = 1;
+
+    while(num>0){
+        fact = fact * num;
+        num--;
+    }
+
+    return fact;
+}
+
+
+const factorial = (num) => {
+    if(num<0) return 0;
+    if(num <= 1) return 1;
+    return num * factorial(num-1)
+}
